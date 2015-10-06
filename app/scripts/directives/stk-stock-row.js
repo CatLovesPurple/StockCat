f90f95da075c6ca770067e5bdc44ae09abaa0ac0
CatLovesPurple <1208bananalas@gmail.com>
2015-10-05 22:00:42 -0700

    Add stock table row and table directives

---
+++ app/scripts/directives/stk-stock-row.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockCatApp.directive:stkStockRow
+ * @description
+ * # stkStockRow
+ */
+angular.module('stockCatApp')
+  .directive('stkStockRow', function ($timeout, QuoteService) {
+    return {
+
+      restrict: 'A',
+      require: '^stkSotckTable',
+      scope:{
+        stock: '=',
+        isLast: '='
+      },
+      link: function postLink($scope, $element, $attrs, stockTableCtrl) {
+        $element.tooltip({
+          placement: 'left',
+          title: $scope.stock.company.name
+        });
+
+        //?? why scope instead of $scope.row????
+        stockTableCtrl.addRow($scope);
+        //register this stock with the QuoteService
+        QuoteService.register($scope.stock);
+
+        $scope.$on('$destroy', function(){
+          stockTableCtrl.removeRow($scope);
+          QuoteService.deregister($scope.stock);
+        });
+
+        //if this is the last row, fetch data from service immediately
+        if($scope.isLast){
+          $timeout(QuoteService.fetch);
+        }
+
+        $scope.$watch('stock.values', function(){
+          $scope.stock.marketValue = $scope.stock.shares * $scope.stock.lastPrice;
+          $scope.stock.dayChange = $scope.stock.shares * parseFloat($scope.stock.change);
+          $scope.stock.save();
+        });
+
+      }
+    };
+  });
